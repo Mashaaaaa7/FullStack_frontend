0659e429b6af8a296e38518daea1302a8260a639
masha <mary200438@gmail.com>
2026-02-14 13:35:37 +0300

    Feature for roles

--- src/components/routes/PrivateRoute.tsx
+++ src/components/routes/PrivateRoute.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../../Context/AuthContext';
-import {Forbidden} from "../Forbidden.tsx";
+import {useAuth} from "../../Context/AuthContext.tsx";
 
-
-type PrivateRouteProps = {
+interface PrivateRouteProps {
     allowedRoles: ('user' | 'admin')[];
-};
+}
 
 export const PrivateRoute: React.FC<PrivateRouteProps> = ({ allowedRoles }) => {
     const { user, loading } = useAuth();
 
     if (loading) return <div>Загрузка...</div>;
 
-    if (!user) {
-        // Если не авторизован, редирект на /login
-        return <Navigate to="/login" replace />;
-    }
+    if (!user) return <Navigate to="/login" replace />;
 
-    if (!allowedRoles.includes(user.role)) {
-        // Если роль не подходит — показываем 403
-        return <Forbidden />;
-    }
+    if (!allowedRoles.includes(user.role)) return <Navigate to="/app" replace />;
 
-    // Всё ок — рендерим дочерние маршруты
     return <Outlet />;
 };
